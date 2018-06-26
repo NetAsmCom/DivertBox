4748d71a8c6b2ee2544d6bc7705957a83e8a14bf
M. Fatih MAR <mfatihmar@gmail.com>
2018-06-26 23:25:50 +0100

    add function parameters

--- divert.h
+++ divert.h
@@ -1,19 +1,19 @@
-#ifndef divert_h
-#define divert_h
+#ifndef DIVERT_H
+#define DIVERT_H
 
-int divert_status();
+int divert_status(int* status);
 
-int divert_open();
-int divert_close();
+int divert_open(void (*handler_func)(int type, void* ip, void* protocol, void* buffer, int length));
+int divert_close(void);
 
-int divert_filter_add();
-int divert_filter_get();
-int divert_filter_remove();
-int divert_filter_count();
-int divert_filter_clear();
+int divert_filter_add(void* buffer, int length);
+int divert_filter_get(int index, void* buffer, int* length);
+int divert_filter_remove(int index);
+int divert_filter_count(int* count);
+int divert_filter_clear(void);
 
-int divert_start();
-int divert_inject();
-int divert_stop();
+int divert_start(int mode);
+int divert_inject(int type, void* ip, void* protocol, void* buffer, int length);
+int divert_stop(void);
 
-#endif // divert_h
+#endif // DIVERT_H
