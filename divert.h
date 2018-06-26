6ccf299cd215de0a4a7593a32945531cb3f33c4c
M. Fatih MAR <mfatihmar@gmail.com>
2018-06-26 23:50:55 +0100

    update buffer pointer types

--- divert.h
+++ divert.h
@@ -3,17 +3,17 @@
 
 int divert_status(int* status);
 
-int divert_open(void (*handler_func)(int type, void* ip, void* protocol, void* buffer, int length));
+int divert_open(void (*handler_func)(int type, void* ip, void* protocol, const char* buffer, int length));
 int divert_close(void);
 
-int divert_filter_add(void* buffer, int length);
-int divert_filter_get(int index, void* buffer, int* length);
+int divert_filter_add(const char* buffer, int length);
+int divert_filter_get(int index, char* buffer, int* length);
 int divert_filter_remove(int index);
 int divert_filter_count(int* count);
 int divert_filter_clear(void);
 
 int divert_start(int mode);
-int divert_inject(int type, void* ip, void* protocol, void* buffer, int length);
+int divert_inject(int type, void* ip, void* protocol, const char* buffer, int length);
 int divert_stop(void);
 
 #endif // DIVERT_H
