b81ce41951a92691ce7806c8f1797856d4514a8c
M. Fatih MAR <mfatihmar@gmail.com>
2018-06-26 01:05:34 +0100

    add basic functions

--- divert.h
+++ divert.h
@@ -1,4 +1,19 @@
 #ifndef divert_h
 #define divert_h
 
+int divert_status();
+
+int divert_open();
+int divert_close();
+
+int divert_filter_add();
+int divert_filter_get();
+int divert_filter_remove();
+int divert_filter_count();
+int divert_filter_clear();
+
+int divert_start();
+int divert_inject();
+int divert_stop();
+
 #endif // divert_h
