22b2e4177c4ef177b0bf5eef0337d2e12c05443e
M. Fatih Marabaoglu <mfatihmar@gmail.com>
2018-06-27 13:10:01 +0100

    typedef enum for type constants

--- divert.h
+++ divert.h
@@ -1,18 +1,21 @@
 #ifndef DIVERT_H
 #define DIVERT_H
 
-#define DIVERT_TYPE_IPV4_UDP    0xA
-#define DIVERT_TYPE_IPV4_ICMP   0xB
-#define DIVERT_TYPE_IPV4_TCP    0xC
-#define DIVERT_TYPE_IPV6_UDP    0xD
-#define DIVERT_TYPE_IPV6_ICMP   0xE
-#define DIVERT_TYPE_IPV6_TCP    0xF
+typedef enum
+{
+    DIVERT_TYPE_IPV4_UDP    = 0xA,
+    DIVERT_TYPE_IPV4_ICMP   = 0xB,
+    DIVERT_TYPE_IPV4_TCP    = 0xC,
+    DIVERT_TYPE_IPV6_UDP    = 0xD,
+    DIVERT_TYPE_IPV6_ICMP   = 0xE,
+    DIVERT_TYPE_IPV6_TCP    = 0xF
+} divert_type_t;
 
 typedef enum
 {
-    DIVERT_MODE_DIVERT          = 0xA,
-    DIVERT_MODE_SNIFF           = 0xB,
-    DIVERT_MODE_INFO            = 0xC
+    DIVERT_MODE_DIVERT      = 0xA,
+    DIVERT_MODE_SNIFF       = 0xB,
+    DIVERT_MODE_INFO        = 0xC
 } divert_mode_t;
 
 int divert_status(int* status);
