6929a61b6ac14a4beeebee09e49002d4e9a6fa8c
M. Fatih MAR <mfatihmar@gmail.com>
2018-07-01 14:09:10 +0100

    create macos kernel extension project

---
+++ macos/extension/divert.c
@@ -0,0 +1,14 @@
+#include <mach/mach_types.h>
+
+kern_return_t divert_start(kmod_info_t * ki, void *d);
+kern_return_t divert_stop(kmod_info_t *ki, void *d);
+
+kern_return_t divert_start(kmod_info_t * ki, void *d)
+{
+    return KERN_SUCCESS;
+}
+
+kern_return_t divert_stop(kmod_info_t *ki, void *d)
+{
+    return KERN_SUCCESS;
+}
