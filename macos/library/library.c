6fe156d140fa361381b80928f62508fc70a6ff56
M. Fatih MAR <mfatihmar@gmail.com>
2018-07-07 20:48:38 +0100

    implement library_chown_directory

--- macos/library/library.c
+++ macos/library/library.c
@@ -1,1 +1,38 @@
 #include "library.h"
+
+#include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
+#include <ftw.h>
+
+uid_t _library_chown_user_id = 0;
+gid_t _library_chown_group_id = 0;
+
+int _library_chown_ftw_handle(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+{
+    if (S_ISDIR(sb->st_mode) || S_ISREG(sb->st_mode))
+    {
+        return chown(fpath, _library_chown_user_id, _library_chown_group_id);
+    }
+
+    return 0;
+}
+
+int library_chown_directory(const char* path, const char* user, const char* group)
+{
+    _library_chown_user_id = 0;
+    {
+        struct passwd* pwd = getpwnam(user);
+        if (pwd == NULL) { return -1; }
+        _library_chown_user_id = pwd->pw_uid;
+    }
+
+    _library_chown_group_id = 0;
+    {
+        struct group* grp = getgrnam(group);
+        if (grp == NULL) { return -1; }
+        _library_chown_group_id = grp->gr_gid;
+    }
+
+    return nftw(path, &_library_chown_ftw_handle, 4, FTW_PHYS | FTW_DEPTH);
+}
--- macos/library/library.h
+++ macos/library/library.h
@@ -4,4 +4,6 @@
 #define kBUNDLE_ID "com.mfatihmar.divert"
 #define kBUNDLE_MD5 "DBA0BB4EA08DD00AD771E99E807B2A86"
 
+int library_chown_directory(const char* path, const char* user, const char* group);
+
 #endif // LIBRARY_H
